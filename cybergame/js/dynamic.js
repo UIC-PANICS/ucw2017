83b5c401910567a43fd2bc0979682be629c920dc
dengrenjie <dengrenjie31@gmail.com>
2017-02-13 23:00:00 +0800

    add animate

--- cybergame/js/dynamic.js
+++ cybergame/js/dynamic.js
@@ -1,33 +1,45 @@
 function phonebar(){
-	var element1=document.getElementById("phonebar");
-	var element2=document.getElementById("phonebarbackground");
-	element1.style.left="0";
-	element2.style.display="block";
-	document.body.style.overflow="hidden";
+	var element1=document.getElementById("phonebar").style;
+	var element2=document.getElementById("phonebarbackground").style;
+	element1.left="0";
+	element2.zIndex="200";
+    element2.backgroundColor="rgba(0,0,0,0.7)";
 }
 
 function hidephonebar(){
-	var element1=document.getElementById("phonebar");
-	var element2=document.getElementById("phonebarbackground");
-	element1.style.left="-200px";
-	element2.style.display="none";
-	document.body.style.overflow="auto";
+	var element1=document.getElementById("phonebar").style;
+	var element2=document.getElementById("phonebarbackground").style;
+	element1.left="-200px";
+	element2.zIndex="-1";
+    element2.backgroundColor="rgba(0,0,0,0)";
 }
 
 function showdetailbox(boxname) {
     var element1=document.getElementById("pcbackground").style;
     var element2=document.getElementById(boxname).style;
-    element1.display="block";
     element2.display="block";
+    element1.backgroundColor="rgba(0,0,0,0.7)";
+    element1.zIndex="200";
     document.body.style.overflow="hidden";
+    setTimeout(function(){
+        var element3=document.getElementById(boxname).style;
+        element3.top="0";
+    },1);
 }
 
 function hidedetailbox(boxname) {
     var element1=document.getElementById("pcbackground").style;
     var element2=document.getElementById(boxname).style;
-    element1.display="none";
     element2.display="none";
+    element1.backgroundColor="rgba(0,0,0,0)";
+    element1.zIndex="-1";
     document.body.style.overflow="auto";
+    if(screen.width>=1024){
+        element2.top="1000px";
+    }
+    else {
+        element2.top = "100px";
+    }
 }
 
 function openpage(link) {
